eca529239825d2bead7ff44b3639c1ed0ef8c2ec
Joe <josephdimartino018@gmail.com>
2021-09-05 15:11:40 +0200

    dockerfile work

--- work.prep/src/main/java/com/dimo/work/prep/Application.java
+++ work.prep/src/main/java/com/dimo/work/prep/Application.java
@@ -1,13 +1,21 @@
 package com.dimo.work.prep;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author jpdho
+ */
 @SpringBootApplication
-public class Application {
+public class Application implements CommandLineRunner {
 
-	public static void main(String[] args) throws InterruptedException {
+	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
 		System.out.println("App Running...");
 		System.out.println("Hello");
 		Thread.sleep(10000);
